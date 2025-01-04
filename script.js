8b851459d5841712c148d3706d947cb7ac1b3c23
KaviyaDharshini31 <kaviyaabi31@gmail.com>
2025-01-04 15:29:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ document.querySelectorAll(".product-card button").forEach(button => {
         alert("Item added to cart!");
     });
 });
+
+// Dynamic Cart Functionality
+let cart = [];
+
+document.querySelectorAll(".product-card button").forEach((button, index) => {
+    button.addEventListener("click", () => {
+        const productCard = button.closest(".product-card");
+        const productName = productCard.querySelector("h3").innerText;
+        const productPrice = productCard.querySelector("p").innerText;
+
+        cart.push({ name: productName, price: productPrice });
+        alert(`${productName} added to the cart!`);
+        console.log("Cart: ", cart);
+    });
+});
